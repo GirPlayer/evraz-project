36ceded2df57565a03e66028e082af114c87b3de
GirPlayer <temusahrik@gmail.com>
2025-04-15 20:00:55 +0500

    рельса 2 все плохо

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -3,9 +3,13 @@
   <q-input v-model="coord" class="def-coor"rounded outlined label="Введите координаты дефектов" />
   <q-btn @click="getInp" class="scan-button" outline rounded color="primary" label="Сканировать" />
   <div class="relsa">
-    <div class="scan-line">
+    <div class="deffects" v-for="coord in coordM" :style="{ 'left' : `${ coord in coordM } px`}">
+    </div>
+
+    <div class="scan-line" :style="{ 'left' : `${ scanPos }px`}">
     </div>
   </div>
+
 </template>
 
 <script setup>
@@ -13,15 +17,27 @@ import { ref } from 'vue'
 
 let l = ref('')
 let coord = ref('')
+let scanPos = ref(0)
+let coordM = ref([])
 
 function getInp() {
-  let coordM = coord.value.split(' ')
+  coordM = coord.value.split(' ')
   if (l.value === ''){
     alert('Введите длину рельсы')
   }
   if (coord.value == ''){
     alert('Введите координаты дефектов')
   }
+  console.log(window.innerWidth)
+
+  let interval = setInterval(() => {
+    scanPos.value = scanPos.value + 10
+
+    if (scanPos.value >= window.innerWidth - 10) {
+      clearInterval(interval)
+    }
+  }, 400);
+
 }
 </script>
 
@@ -44,11 +60,22 @@ function getInp() {
   width: 100%;
   height: 150px;
   background-color: gray;
+  position: relative;
 }
 
 .scan-line{
   width: 10px;
   height: 150px;
   background: red;
+  position: absolute;
+  left: 0px;
+}
+
+.deffects{
+  height: 20px;
+  width: 20px;
+  background: lawngreen;
+  border-radius: 50%;
+  position: absolute;
 }
 </style>
